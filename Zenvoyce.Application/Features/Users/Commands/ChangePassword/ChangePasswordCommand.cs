609636e7ac76102bafe3e12c6966078e27749fab
tuan-haui <2022605037.dhk17@st.haui.edu.vn>
2026-05-05 21:06:43 +0700

    update

--- Zenvoyce.Application/Features/Users/Commands/ChangePassword/ChangePasswordCommand.cs
+++ Zenvoyce.Application/Features/Users/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -5,14 +5,13 @@ using Zenvoyce.Domain.Interfaces;
 
 namespace Zenvoyce.Application.Features.Users.Commands.ChangePassword;
 
-public record ChangePasswordCommand(Guid Id, string OldPassword, string NewPassword) : IRequest<Unit>;
+public record ChangePasswordCommand(Guid Id, string NewPassword) : IRequest<Unit>;
 
 public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
 {
     public ChangePasswordCommandValidator()
     {
         RuleFor(x => x.Id).NotEmpty();
-        RuleFor(x => x.OldPassword).NotEmpty();
         RuleFor(x => x.NewPassword)
             .NotEmpty()
             .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$");
@@ -29,11 +28,6 @@ public class ChangePasswordCommandHandler(
     {
         var user = await userRepository.GetByIdAsync(request.Id, cancellationToken)
             ?? throw new KeyNotFoundException("Không tìm thấy tài khoản.");
-
-        if (!passwordHasher.Verify(request.OldPassword, user.Matkhau))
-        {
-            throw new InvalidOperationException("Mật khẩu cũ không chính xác.");
-        }
 
         user.Matkhau = passwordHasher.Hash(request.NewPassword);
         user.UpdatedAt = dateTimeProvider.UtcNow;
